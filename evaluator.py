5e9d9f5d99eb0583bff494cdaa4aa9123bd25dc3
Dustin Getz <dustin.getz@gmail.com>
2012-08-17 19:14:16 -0400

    broke apart eval

--- evaluator.py
+++ evaluator.py
@@ -27,72 +27,101 @@ class Env(dict):
         return self if var in self else self.outer.find(var)
 
 
+def eval_symbol(x, env):
+    assert env
+    return ok(env.find(x)[x])
+
+def eval_literal(x):
+    return ok(x)
+
+def eval_quote(x):
+    (_, exp) = x
+    return ok(exp)
+
+def eval_if(x, env):
+    (_, test, conseq, alt) = x
+    def _doif(val):
+        return (conseq if val else alt)
+    mbranch = fmap(_doif, eval(test, env))
+    return bind(mbranch, lambda branch: eval(branch, env))
+
+def eval_set(x, env):
+    (_, var, exp) = x
+    def _doset(val):
+        #assert env.find(var) -- this is a RuntimeError
+        assert env
+        env.find(var)[var] = val
+        return None
+    return fmap(_doset, eval(exp, env))
+
+def eval_define(x, env):
+    (_, var, exp) = x
+    def _dodefine(val):
+        env[var] = val
+        return None
+    return fmap(_dodefine, eval(exp, env))
+
+def eval_lambda(x, env):
+    (_, vars, exp) = x
+    return ok(lambda *args: eval(exp, Env(vars, args, env)))
 
 
-isa = isinstance
+def eval_begin(x, env):
+    exprs = x[1:]
+
+    # this code is a recursive monad comprehension, it composes the plumbing
+    # such that errors short circuit etc, but discards the passed in value
+    # and just evaluates the next expression. the return value is monadic -
+    # the last expression evaluated.
+    #
+    # version if there wasn't a monad:
+    #
+    #   for exp in x[1:]:
+    #     val = eval(exp, env)
+    #   return val
+    #
+    def _dobegin(exp, exprs): # -> mv
+        mfirst = eval(exp, env)
+        if not exprs: return mfirst
+        return bind(mfirst, lambda _: _dobegin(exprs[0], exprs[1:]))
+    return _dobegin(exprs[0], exprs[1:])
+
+def eval_proc(x, env):
+    mvs = map(lambda exp: eval(exp, env), x)
+    mproc, margs = mvs.pop(0), mvs
+    args = seq(margs)
+    return bind(mproc, lambda proc: proc(*args))
+
+
+
+
 
 def eval(x, env):
     "Evaluate an expression in an environment."
-    if isa(x, Symbol):             # variable reference
-        assert env
-        return ok(env.find(x)[x])
 
-    elif not isa(x, list):         # constant literal
-        return ok(x)
+    if isinstance(x, Symbol):
+        return eval_symbol(x, env)
 
-    elif x[0] == 'quote':          # (quote exp)
-        (_, exp) = x
-        return ok(exp)
+    elif not isinstance(x, list):
+        return eval_literal(x)
+
+    elif x[0] == 'quote':
+        return eval_quote(x)
 
     elif x[0] == 'if':             # (if test conseq alt)
-        (_, test, conseq, alt) = x
-        def _doif(val):
-            return (conseq if val else alt)
-        mbranch = fmap(_doif, eval(test, env))
-        return bind(mbranch, lambda branch: eval(branch, env))
+        return eval_if(x, env)
 
     elif x[0] == 'set!':           # (set! var exp)
-        (_, var, exp) = x
-        def _doset(val):
-            #assert env.find(var) -- this is a RuntimeError
-            assert env
-            env.find(var)[var] = val
-            return None
-        return fmap(_doset, eval(exp, env))
+        return eval_set(x, env)
 
     elif x[0] == 'define':         # (define var exp)
-        (_, var, exp) = x
-        def _dodefine(val):
-            env[var] = val
-            return None
-        return fmap(_dodefine, eval(exp, env))
+        return eval_define(x, env)
 
     elif x[0] == 'lambda':         # (lambda (var*) exp)
-        (_, vars, exp) = x
-        return ok(lambda *args: eval(exp, Env(vars, args, env)))
+        return eval_lambda(x, env)
 
     elif x[0] == 'begin':          # (begin exp*)
-        exprs = x[1:]
-
-        # this code is a recursive monad comprehension, it composes the plumbing
-        # such that errors short circuit etc, but discards the passed in value
-        # and just evaluates the next expression. the return value is monadic -
-        # the last expression evaluated.
-        #
-        # version if there wasn't a monad:
-        #
-        #   for exp in x[1:]:
-        #     val = eval(exp, env)
-        #   return val
-        #
-        def _dobegin(exp, exprs): # -> mv
-            mfirst = eval(exp, env)
-            if not exprs: return mfirst
-            return bind(mfirst, lambda _: _dobegin(exprs[0], exprs[1:]))
-        return _dobegin(exprs[0], exprs[1:])
+        return eval_begin(x, env)
 
     else:                          # (proc exp*)
-        mvs = map(lambda exp: eval(exp, env), x)
-        mproc, margs = mvs.pop(0), mvs
-        args = seq(margs)
-        return bind(mproc, lambda proc: proc(*args))
+        return eval_proc(x, env)
